b691ae4e77aff4ee6aec71d9432babc024f2553c
Anchita Bora <anchita.bora@gmail.com>
2022-10-31 20:52:01 -0700

    created helper file with fns to find 0 tile, check goal state, and print the puzzle

---
+++ helpers.py
@@ -0,0 +1,36 @@
+
+#Helper tp find all moves and respective costs. WIll return the board after the move.
+from copy import deepcopy
+
+
+def get_all_moves(board):
+    def find_zero():
+        zero_index = None
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if board[i][j] == 0:
+                    zero_index = (i,j)
+                    return zero_index
+    row, col = find_zero()
+    moves = []
+
+    #Left
+    if col != 0:
+        new_board = deepcopy(board)
+        new_board[row][col-1], new_board[row][col] = board[row][col], board[row][col-1]
+        moves.append((new_board, 1))
+
+    return moves
+
+def is_goal_state(board):
+    goal_state = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
+    return board == goal_state
+
+#Helper from example slides [TODO! insert slide deck name]
+def print_puzzle(puzzle):
+    for i in range(0, 3):
+        print(puzzle[i])
+    print('\n')
+
+
+
